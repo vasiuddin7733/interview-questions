17950d16e11f9cc0296939a58d24359d76b55877
vasiuddin <vasiuddin.mohammed1@gmail.com>
2026-03-26 14:32:16 +0530

    file structure config change

--- ui/src/app/page.js
+++ ui/src/app/page.js
@@ -1,4 +1,3 @@
-import Closure from "@/components/Closure";
 import Image from "next/image";
 
 export default function Home() {
@@ -15,7 +14,7 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+            To get started, edit the page.js file.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
@@ -59,7 +58,6 @@ export default function Home() {
           >
             Documentation
           </a>
-          <Closure />
         </div>
       </main>
     </div>
